908a8f04983953ef66ac3f1076aaa738ece7b117
Langton49 <munashemukweya2022@gmail.com>
2025-11-15 23:53:57 -0600

    merged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
-import { useState } from 'react'
 import './App.css'
 import Dashboard from './pages/Dashboard.jsx';
 import { useRoutes } from 'react-router-dom'
 
 function App() {
-    let element = useRoutes([
-        {
-          path:'/dashboard',
-          element: <Dashboard />
-        },
-        {
-            path:'/createhabit',
-            element: <Dashboard />
-          },
-          {
-            path:'/createtask',
-            element: <Dashboard />
-          },
-      ])
-    
-      return (
-        <div className='app'>
-            { element }
-        </div>
-      )
-    }
-    
-    export default App
+  let element = useRoutes([
+    {
+      path: '/dashboard',
+      element: <Dashboard />
+    },
+    {
+      path: '/createhabit',
+      element: <Dashboard />
+    },
+    {
+      path: '/createtask',
+      element: <Dashboard />
+    },
+  ])
+
+  return (
+    <div className='app'>
+      {element}
+    </div>
+  )
+}
+
+export default App
